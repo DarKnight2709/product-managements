25c51d6772dfd476ef9c05398851f45ee01c9fe7
quyentran <tranduyquyen1234567890@gmail.com>
2025-07-16 16:57:22 +0700

    Tính năng tìm kiếm sản phẩm - admin
    sử dụng regex -> xem thêm về regex

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -40,12 +40,22 @@ exports.index =  async (req, res) => {
     find.status = req.query.status;
   }
 
+  let keyword = "";
+  console.log(keyword);
+  if(req.query.keyword) {
+    keyword = req.query.keyword;
+    const regex = new RegExp(keyword, 'i');
+    find.title = regex;
+  }
+  console.log(find);
+
   const products = await Product.find(find);
   // console.log(products);
   res.render("admin/pages/products/index.pug", {
         pageTitle: "Trang danh sách sản phẩm",
         products: products,
         filterStatus: filterStatus,
+        keyword: keyword
     });
 
   
